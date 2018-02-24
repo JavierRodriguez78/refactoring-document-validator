8eb47ba7cf360979c1e6a6aceafe1e106f1e60be
Miguel Ángel Sánchez Chordi <slayer.ma@gmail.com>
2018-02-24 14:22:25 +0100

    Extract constant

--- src/NifValidator.php
+++ src/NifValidator.php
@@ -5,6 +5,7 @@ namespace DocumentValidator;
 class NifValidator extends Validator
 {
     const NIF_REGEX = '/^[KLM0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][a-zA-Z0-9]/';
+    const NIF_LETTERS = 'TRWAGMYFPDXBNJZSQVHLCKE';
 
     public function isValid($number)
     {
@@ -28,7 +29,6 @@ class NifValidator extends Validator
 
     private function getNIFCheckDigit($number)
     {
-        $keyString = 'TRWAGMYFPDXBNJZSQVHLCKE';
         $correctLetter = "";
 
         if ($this->isValidNIFFormat($number)) {
@@ -36,7 +36,7 @@ class NifValidator extends Validator
             $number = str_replace('L', '0', $number);
             $number = str_replace('M', '0', $number);
             $position = substr($number, 0, 8) % 23;
-            $correctLetter = substr($keyString, $position, 1);
+            $correctLetter = substr(self::NIF_LETTERS, $position, 1);
         }
 
         return $correctLetter;
