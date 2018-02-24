879aaf3fe32e8763bf0a8b5f2c778e217d92c6ca
Miguel Ángel Sánchez Chordi <slayer.ma@gmail.com>
2018-02-24 14:15:53 +0100

    Replace conditional with factory method

--- src/DocumentValidator.php
+++ src/DocumentValidator.php
@@ -4,23 +4,32 @@ namespace DocumentValidator;
 
 class DocumentValidator
 {
+    private $validators;
+
+    public function __construct()
+    {
+        $this->validators = [
+            'NIF' => new NifValidator(),
+            'NIE' => new NieValidator(),
+            'CIF' => new CifValidator(),
+        ];
+    }
+
     public function isValidIdNumber($docNumber, $type)
     {
-        $fixedDocNumber = strtoupper($docNumber);
-        $fixedType = strtoupper($type);
+        $normalizedType = strtoupper($type);
 
-        switch ($fixedType) {
-            case 'NIF':
-                $validator = new NifValidator();
-                return $validator->isValid($fixedDocNumber);
-            case 'NIE':
-                $validator = new NieValidator();
-                return $validator->isValid($fixedDocNumber);
-            case 'CIF':
-                $validator = new CifValidator();
-                return $validator->isValid($fixedDocNumber);
-            default:
-                throw new \Exception('Unsupported Type');
+        $validator = $this->getValidator($normalizedType);
+
+        return $validator->isValid($docNumber);
+    }
+
+    private function getValidator($type)
+    {
+        if (!array_key_exists($type, $this->validators)) {
+            throw new \Exception('Unsupported Type');
         }
+
+        return $this->validators[$type];
     }
 }
